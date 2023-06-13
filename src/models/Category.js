6d12bc4060ebfb9985dc487b2c3014c6856a866b
gabesouto <soutogabriel04@gmail.com>
2023-06-13 19:21:15 -0300

    Requisito 07

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+
+
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes,
+      allowNull: false,
+    },
+  })
+  return Category
+}
+  
+
+
